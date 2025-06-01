039ae72a561035abdb59a8279c2189afba24ce73
Edwman2 <122057797+Edwman2@users.noreply.github.com>
2025-06-01 21:33:11 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import  './Contact.css'
-import {HashRouter as Router} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Portfolio from './pages/Portfolio'
